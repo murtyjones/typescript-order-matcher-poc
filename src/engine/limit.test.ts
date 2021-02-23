ec8ea9fed3740f8adc90879da5bf7fd2eaec545d
Marty Jones <murtin.jones@gmail.com>
2021-02-23 17:34:48 -0500

    Add more test coverage

--- src/engine/limit.test.ts
+++ src/engine/limit.test.ts
@@ -48,7 +48,7 @@ describe('Limit order', () => {
   });
 
   describe('a buy fully covered by multiple sells', () => {
-    it('should fully execute when buy happens first', () => {
+    it('should fully execute when buying happens first', () => {
       const buy: IOrder = { id: 1, amount: 100, price: 1.29, side: 'buy' };
       const sells: IOrder[] = [
         { id: 2, amount: 33, price: 1.29, side: 'sell' },
@@ -66,7 +66,7 @@ describe('Limit order', () => {
       expect(limit.book.sells[0]!.amount).toBe(1);
     });
 
-    it('should fully execute when buy happens second', () => {
+    it('should fully execute when buying happens second', () => {
       const buy: IOrder = { id: 1, amount: 100, price: 1.29, side: 'buy' };
       const sells: IOrder[] = [
         { id: 2, amount: 33, price: 1.29, side: 'sell' },
@@ -85,8 +85,48 @@ describe('Limit order', () => {
     });
   });
 
+  describe('a sell fully covered by multiple buys', () => {
+    it('should fully execute when buying happens first', () => {
+      const sell: IOrder = { id: 1, amount: 100, price: 1.29, side: 'sell' };
+      const buys: IOrder[] = [
+        { id: 2, amount: 1, price: 1.29, side: 'buy' },
+        { id: 3, amount: 20, price: 1.3, side: 'buy' },
+        { id: 4, amount: 90, price: 1.31, side: 'buy' },
+      ];
+      const limit = new Limit(new OrderBook());
+      buys.forEach((each) => {
+        limit.process(each);
+      });
+      limit.process(sell);
+      expect(limit.book.sells.length).toBe(0);
+      expect(limit.book.buys.length).toBe(2);
+      expect(limit.book.buys[0]!.price).toBe(1.29);
+      expect(limit.book.buys[0]!.amount).toBe(1);
+      expect(limit.book.buys[1]!.price).toBe(1.3);
+      expect(limit.book.buys[1]!.amount).toBe(10);
+    });
+
+    it('should fully execute when buying happens second', () => {
+      const sell: IOrder = { id: 1, amount: 100, price: 1.29, side: 'sell' };
+      const buys: IOrder[] = [
+        { id: 2, amount: 1, price: 1.29, side: 'buy' },
+        { id: 3, amount: 20, price: 1.3, side: 'buy' },
+        { id: 4, amount: 90, price: 1.31, side: 'buy' },
+      ];
+      const limit = new Limit(new OrderBook());
+      limit.process(sell);
+      buys.forEach((each) => {
+        limit.process(each);
+      });
+      expect(limit.book.sells.length).toBe(0);
+      expect(limit.book.buys.length).toBe(1);
+      expect(limit.book.buys[0]!.price).toBe(1.31);
+      expect(limit.book.buys[0]!.amount).toBe(11);
+    });
+  });
+
   describe('a buy partially covered by multiple sells', () => {
-    it('should partially execute when buy happens first', () => {
+    it('should partially execute when buying happens first', () => {
       const buy: IOrder = { id: 1, amount: 100, price: 1.29, side: 'buy' };
       const sells: IOrder[] = [
         { id: 2, amount: 1, price: 1.29, side: 'sell' },
@@ -105,7 +145,7 @@ describe('Limit order', () => {
       expect(limit.book.sells[0]!.amount).toBe(20);
     });
 
-    it('should partially execute when buy happens second', () => {
+    it('should partially execute when buying happens second', () => {
       const buy: IOrder = { id: 1, amount: 100, price: 1.29, side: 'buy' };
       const sells: IOrder[] = [
         { id: 2, amount: 1, price: 1.29, side: 'sell' },
@@ -122,6 +162,48 @@ describe('Limit order', () => {
       expect(limit.book.sells.length).toBe(1);
       expect(limit.book.sells[0]!.price).toBe(1.3);
       expect(limit.book.sells[0]!.amount).toBe(20);
+    });
+  });
+
+  describe('a sell partially covered by multiple buys', () => {
+    it('should partially execute when buying happens first', () => {
+      const sell: IOrder = { id: 1, amount: 100, price: 1.29, side: 'sell' };
+      const buys: IOrder[] = [
+        { id: 2, amount: 1, price: 1.28, side: 'buy' },
+        { id: 2, amount: 1, price: 1.29, side: 'buy' },
+        { id: 3, amount: 20, price: 1.3, side: 'buy' },
+      ];
+      const limit = new Limit(new OrderBook());
+      buys.forEach((each) => {
+        limit.process(each);
+      });
+      limit.process(sell);
+      expect(limit.book.sells.length).toBe(1);
+      expect(limit.book.sells[0]!.price).toBe(1.29);
+      expect(limit.book.sells[0]!.amount).toBe(79);
+      expect(limit.book.buys.length).toBe(1);
+      expect(limit.book.buys[0]!.price).toBe(1.28);
+      expect(limit.book.buys[0]!.amount).toBe(1);
+    });
+
+    it('should partially execute when buying happens second', () => {
+      const sell: IOrder = { id: 1, amount: 100, price: 1.29, side: 'sell' };
+      const buys: IOrder[] = [
+        { id: 2, amount: 1, price: 1.28, side: 'buy' },
+        { id: 2, amount: 1, price: 1.29, side: 'buy' },
+        { id: 3, amount: 20, price: 1.3, side: 'buy' },
+      ];
+      const limit = new Limit(new OrderBook());
+      limit.process(sell);
+      buys.forEach((each) => {
+        limit.process(each);
+      });
+      expect(limit.book.sells.length).toBe(1);
+      expect(limit.book.sells[0]!.price).toBe(1.29);
+      expect(limit.book.sells[0]!.amount).toBe(79);
+      expect(limit.book.buys.length).toBe(1);
+      expect(limit.book.buys[0]!.price).toBe(1.28);
+      expect(limit.book.buys[0]!.amount).toBe(1);
     });
   });
 });
