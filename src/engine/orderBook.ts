e5fb9ae0cf3845c5bf8f01f6c94fa4a66065c3e9
Marty Jones <murtin.jones@gmail.com>
2021-02-23 20:25:16 -0500

    Remove unneeded logic

--- src/engine/orderBook.ts
+++ src/engine/orderBook.ts
@@ -28,13 +28,7 @@ export class OrderBook implements IOrderBook {
         break;
       }
     }
-    if (i == l - 1) {
-      // Put order at end of book
-      this.buys.push(order);
-    } else {
-      // Otherwise, put order into array
-      this.buys.splice(i + 1, 0, order);
-    }
+    this.buys.splice(i + 1, 0, order);
   };
 
   private addSellOrder = (order: Order): void => {
@@ -46,11 +40,7 @@ export class OrderBook implements IOrderBook {
         break;
       }
     }
-    if (i == l - 1) {
-      this.sells.push(order);
-    } else {
-      this.sells.splice(i + 1, 0, order);
-    }
+    this.sells.splice(i + 1, 0, order);
   };
 
   removeBuyOrder = (index: number): void => {
